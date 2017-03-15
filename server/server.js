494053f3a21a5365aa9e0b6f969dc1b3a1af7be9
Christine Dey <christinedey5@gmail.com>
2017-03-14 20:06:33 -0700

    Update server template

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
   res.send('Hello World!')
 });
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000!')
+app.listen(process.env.PORT, function() {
+  console.log('Listening on port ', process.env.PORT);
 });
