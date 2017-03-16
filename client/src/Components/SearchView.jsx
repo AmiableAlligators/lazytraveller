0f63b2f115148d103bbec84555e64d67f3f809da
Winston Ku <wdku5791@gmail.com>
2017-03-16 14:33:45 -0700

    reverted changes in approuter.js to router

--- client/src/Components/SearchView.jsx
+++ client/src/Components/SearchView.jsx
@@ -10,6 +10,7 @@ export default class SearchView extends React.Component {
 	render () {
 		return (
 			<div className="appNav">
+      <h1>Lazy Traveller</h1>
 				<div>
 					<SearchBar />
 				</div>
