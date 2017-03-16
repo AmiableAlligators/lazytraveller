3673030a2c92047bdbaa8cc7e6b0de0f2607a434
Terence Tham <terence@epiphanyedu.com>
2017-03-16 15:59:15 -0700

    Combine and update Client React Components

--- client/src/components/ActivityView.jsx
+++ client/src/components/ActivityView.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const ActivityView = (props) => {
+const ActivityView = ({ activity }) => {
   <div class="activity-container">
-    <div class="activity-title">{ props.activity.title }</div>
+    <div class="activity-title">{ activity.title }</div>
     <div class="activity-img">
-      <img src={ props.activity.url } alt="" />
+      <img src={ activity.url } alt="" />
     </div>
-    <div class="activity-des">{ props.activity.description }</div>
-    <div class="activity-dist">{ props.activity.dist }</div>
-    <div class="activity-price">{ props.activity.price }</div>
+    <div class="activity-des">{ activity.description }</div>
+    <div class="activity-dist">{ activity.dist }</div>
+    <div class="activity-price">{ activity.price }</div>
     <button class="like"
-      onClick={ () => handleLikeClick( true, props.activity.activityId ) }>Like
+      onClick={ () => handleLikeClick( true, activity.activityId ) }>Like
     </button>
     <button class="dis-like"
-      onClick={ () => handleLikeClick( false, props.activity.activityId ) }>Dislike
+      onClick={ () => handleLikeClick( false, activity.activityId ) }>Dislike
     </button>
   </div>
 };
