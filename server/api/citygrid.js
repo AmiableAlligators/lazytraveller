64bbb351d009ca08dfef0f3d5dea8cdefa2a7df9
Christine Dey <christinedey5@gmail.com>
2017-03-16 19:18:09 -0700

    Remove extra citygrid file

--- server/api/citygrid.js
+++ server/api/citygrid.js
@@ -1,20 +1,41 @@
-const request = require('request');
+const Promise = require('bluebird');
+const request = Promise.promisify(require('request'));
+
+// module.exports = {
+// 	fetch: function(place, callback) {
+//     var searchObj = {
+//       where: place,
+//       type: 'travel',
+//       sort: 'highestrated' 
+//     };
+// 		request('https://api.citygridmedia.com/content/places/v2/search/where?type=' + searchObj.type + '&where=' + searchObj.place + '&sort=' + searchObj.sort + '&publisher=christinedey5@gmail.com', function (err, response) {
+//       if (err) {
+//         console.log(err, null);
+//       } else {
+//         callback(null, response);
+//       }
+// 		});	
+// 	} 
+// }
 
 module.exports = {
-	fetch: function(place, callback) {
+  fetch: function(place) {
     var searchObj = {
       where: place,
       type: 'travel',
       sort: 'highestrated' 
     };
-		request('https://api.citygridmedia.com/content/places/v2/search/where?type=travel&where=' + place + '&sort=highestrated&publisher=christinedey5@gmail.com', function (err, response) {
-      if (err) {
-        console.log(err, null);
-      } else {
-        callback(null, response);
-      }
-		});	
-	} 
+
+    return new Promise (function(resolve, reject) {
+      request('https://api.citygridmedia.com/content/places/v2/search/where?type=' + searchObj.type + '&where=' + searchObj.place + '&sort=' + searchObj.sort + '&publisher=christinedey5@gmail.com', function(error, response) {
+        if (error) {
+          reject(error);         
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  } 
 }
 
 //should return a promise instead of callback 
