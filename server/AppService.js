ffeba07283e4da1a3cc322db8bafe86b5c1f1797
Li Yi <jayceeyili@gmail.com>
2017-03-16 19:50:33 -0700

    query: send example data back to client

---
+++ server/AppService.js
@@ -0,0 +1,16 @@
+const citygrid = require('./api/citygrid');
+
+// const AppService = {
+//   fetch: function(query, callback) {
+//     citygrid.fetch(query, function(err, data) {
+//       if (err) {
+//         callback(err, null);
+//       } else {
+//         callback(null, data);
+//       }
+//     })
+//   }
+// };
+
+
+module.exports = AppService;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+const data = require('./../data/sampleData');
 
 var port = process.env.PORT || 3000;
 
@@ -10,6 +12,16 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/public'));
 
+app.post('/query', function(req, res) {
+  // AppService.fetch(req.body)
+    // .then(function(data) {
+    //   res.json(data);
+    // })
+    // .error(function(error) {
+    //   res.send(error);
+    // })
+  res.json(data);
+});
 
 // ««««««««« start app  »»»»»»»»»
 app.listen(port, function() {
