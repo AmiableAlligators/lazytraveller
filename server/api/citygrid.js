12bab040dc4b8b26f9cd1348cfd07dc969072d35
Terence Tham <terence@epiphanyedu.com>
2017-03-20 20:32:20 -0700

    Update project to use environment variables.
    1. Updated example.env - create your own .env file in project root
    2. Installed dotenv to manage environment variables
    3. Updated publisher reference in citygrid API to use environment variable

--- server/api/citygrid.js
+++ server/api/citygrid.js
@@ -10,15 +10,13 @@ module.exports = {
     };
 
     return new Promise (function(resolve, reject) {
-      let requestString = 
-        `http://api.citygridmedia.com/content/places/v2/search/where?type=${search.type}&where=${search.where}&what=places%20to%20visit%20&sort=${search.sort}&format=json&publisher=10000019378`;
-
+      let requestString = `http://api.citygridmedia.com/content/places/v2/search/where?type=${search.type}&where=${search.where}&what=places%20to%20visit%20&sort=${search.sort}&format=json&publisher=${process.env.CITYGRID_PUBLISHER}`;
       request(requestString, function(error, response) {
         if (error) {
           console.error(error);
           reject(error);
         } else {
-          console.log('a response-----',response.body);
+          // console.log('a response-----',response.body);
           resolve(response.body);
         }
       });
