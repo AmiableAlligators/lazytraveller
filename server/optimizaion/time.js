551947f540c94e1ded566d5993f67bac7864b57e
Li Yi <jayceeyili@gmail.com>
2017-03-23 16:12:28 -0700

    include some guess about estimatedDuration

--- server/optimizaion/time.js
+++ server/optimizaion/time.js
@@ -21,3 +21,22 @@ const timeOptimization = (duration, locations) => {
 
   return result;
 };
+
+// Unit of estimatedDuration: hours
+
+// {name: "Museums", type: "city"}}, --estimatedDuration: 6
+// {name: "Amusement Parks", type: "city"}},--estimatedDuration: 4
+// {name: "Aquariums", type: "city"}},--estimatedDuration: 3
+// {name: "Zoos", type: "city"}},--estimatedDuration: 6
+// {name: "Shopping Centers & Malls", type: "city"}},--estimatedDuration: 0.5
+// {name: "Art Centers", type: "city"}},--estimatedDuration: 5
+// {name: "Historical Sites", type: "city"}},--estimatedDuration: 3
+// {name: "Cultural Centers", type: "city"}},--estimatedDuration: 3
+// {name: "Botanical Gardens", type: "outdoor"}},--estimatedDuration: 2
+// {name: "Hot Springs", type: "outdoor"}},--estimatedDuration: 8
+// {name: "Beaches", type: "outdoor"}},--estimatedDuration: 4
+// {name: "National Parks", type: "outdoor"}},--estimatedDuration: 8
+// {name: "Hiking Trails", type: "outdoor"}},--estimatedDuration: 4
+// {name: "Water Parks", type: "outdoor"}},--estimatedDuration: 3
+// {name: "Outdoors", type: "outdoor"}},--estimatedDuration: 4
+// {name: "Attractions", type: "city"}}--estimatedDuration: 2
