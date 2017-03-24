0e20e4a5ad3a9d7170509c980e6d145a688e48ce
Terence Tham <terence@epiphanyedu.com>
2017-03-24 16:42:19 -0700

    Update feature to save activities into the database.
    1. Activities are saved only if not available in the DB
    2. Those that are in the DB get updated with new activities data
    3. Installed async package to use async map to append Mongoose _ids to the returned activity data

--- server/AppService.js
+++ server/AppService.js
@@ -3,6 +3,7 @@ const yelp = require('./api/yelp');
 const _ = require('underscore');
 const priceBased = require('./optimizaion/price');
 const Activities = require('./db/Activities.js');
+const asyncMap = require('async/map');
 
 const priceLimit = 4; // hard coded for now, this should be a number from 1 to 4
 
@@ -27,16 +28,27 @@ const AppService = {
     });
 
     return Promise.all(fetches)
-    .then(apiResults => {
-      return new Promise((resolve, reject) => {
-        // resolve(priceBased(unique(apiResults), priceLimitation)); // for testing
-        unique(apiResults).map(result => {
-          Activities.add(result);   
+      .then(apiResults => {
+        return new Promise((resolve, reject) => {
+          // resolve(priceBased(unique(apiResults), priceLimitation)); // for testing
+          asyncMap(unique(apiResults), 
+            (item, transformed) => {
+              let query = {
+                name: item.name,
+                'address.city': item.address.city
+              }
+              Activities.findAndUpdate(query, item, function(insertResult) {
+                item._id = insertResult._id; 
+                transformed(null, item);
+              });   
+            },
+            (err, results) => {
+              resolve(results);
+            }
+          );
         });
-        resolve(unique(apiResults));
-      });
-    })
-    .catch(error => console.error(error)); 
+      })
+      .catch(error => console.error(error)); 
   }
 };
 
